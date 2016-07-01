adef6842aa5b6b3d94169c520ffc996933676e11
shadd0w <rene.gmitter@wame.sk>
2016-07-01 15:53:14 +0200

    POLISH CategoryPresenter

--- presenters/CategoryPresenter.php
+++ presenters/CategoryPresenter.php
@@ -4,11 +4,7 @@ namespace App\CategoryModule\Presenters;
 
 use Wame\CategoryModule\Repositories\CategoryRepository;
 use Wame\CategoryModule\Repositories\CategoryLangRepository;
-//use Wame\CategoryModule\Repositories\CategoryItemRepository;
 use Wame\CategoryModule\Components\ICategoryListControlFactory;
-use Wame\ArticleCategoryPlugin\Controls\ArticlesOfCategory;
-
-use Wame\ArticleCategoryPlugin\Repositories\ArticleCategoryRepository;
 
 
 class CategoryPresenter extends \App\Core\Presenters\BasePresenter
@@ -19,36 +15,12 @@ class CategoryPresenter extends \App\Core\Presenters\BasePresenter
 	/** @var CategoryLangRepository @inject */
 	public $categoryLangRepository;
 	
-//	/** @var CategoryItemRepository @inject */
-//	public $categoryItemRepository;
-	
-	/** @var ArticlesOfCategory @inject */
-	public $articlesOfCategory;
-	
 	/** @var ICategoryListControlFactory @inject */
 	public $ICategoryListControlFactory;
-	
-	/** @var ArticleCategoryRepository @inject */
-	public $articleCategoryRepository;
 	
 	/** @var array */
 	public $items = [];
 	
-	
-//	public function renderArticleCategories()
-//	{
-//		$articleCategories = $this->articleCategoryRepository->find();
-//	}
-	
-//	public function renderArticles()
-//	{
-//		$this->template->items = $this->items;
-//	}
-	
-//	public function createComponentArticlesOfCategory()
-//	{
-//		return $this->articlesOfCategory;
-//	}
 	
 	/** components ************************************************************/
 	
