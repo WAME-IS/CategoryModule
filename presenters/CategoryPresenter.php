55bfcc9141fcfff24771789b4e8e0fe125ad9508
shadd0w <rene.gmitter@wame.sk>
2016-07-08 16:45:20 +0200

    UPDATE CategoryModule

--- presenters/CategoryPresenter.php
+++ presenters/CategoryPresenter.php
@@ -4,7 +4,10 @@ namespace App\CategoryModule\Presenters;
 
 use Wame\CategoryModule\Repositories\CategoryRepository;
 use Wame\CategoryModule\Repositories\CategoryLangRepository;
+use Wame\CategoryModule\Repositories\CategoryItemRepository;
 use Wame\CategoryModule\Components\ICategoryListControlFactory;
+
+use Wame\ListByTypeControl\Components\IListByTypeControlFactory;
 
 
 class CategoryPresenter extends \App\Core\Presenters\BasePresenter
@@ -14,13 +17,40 @@ class CategoryPresenter extends \App\Core\Presenters\BasePresenter
 
 	/** @var CategoryLangRepository @inject */
 	public $categoryLangRepository;
+    
+    /** @var CategoryItemRepository @inject */
+    public $categoryItemRepository;
 	
 	/** @var ICategoryListControlFactory @inject */
 	public $ICategoryListControlFactory;
-	
+    
+    /** @var IListByTypeControlFactory @inject */
+    public $IListByTypeControlFactory;
+    
 	/** @var array */
 	public $items = [];
-	
+    
+    /** @var integer */
+    public $selectedCategory;
+    
+    /** @var string */
+    public $type;
+    
+    
+    public function actionShow($id)
+    {
+        $type = $this->getParameter('type');
+        
+        try {
+            $category = $this->categoryRepository->get(['id' => $id]);
+            $categoryItems = $this->categoryItemRepository->getItems($type, $this->id);
+        } catch(\Exception $e) {
+            
+        } finally {
+//            $this->redirect(404, ':Homepage:Homepage:');
+        }
+    }
+    
 	
 	/** components ************************************************************/
 	
@@ -30,5 +60,12 @@ class CategoryPresenter extends \App\Core\Presenters\BasePresenter
 		
 		return $component;
 	}
+    
+    public function createComponentListByType()
+    {
+        $component = $this->IListByTypeControlFactory->create();
+        
+        return $component;
+    }
 	
 }
