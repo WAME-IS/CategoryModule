287ff883d30e9a20f0d2e12eda71bc056e31a35a
Stanislav Červeňák <stano@wame.sk>
2016-10-07 12:17:51 +0200

    EDIT form elements width

--- forms/groups/category/CategoryGroup.php
+++ forms/groups/category/CategoryGroup.php
@@ -4,7 +4,7 @@ namespace Wame\CategoryModule\Forms\Groups;
 
 use Wame\DynamicObject\Registers\Types\IBaseContainer;
 use Wame\DynamicObject\Forms\Groups\BaseGroup;
-use Nette\Utils\Html;
+
 
 interface ICategoryGroupFactory extends IBaseContainer
 {
@@ -12,19 +12,21 @@ interface ICategoryGroupFactory extends IBaseContainer
 	function create();
 }
 
+
 class CategoryGroup extends BaseGroup
 {
     public function __construct()
     {
         parent::__construct();
-        $this->setAttribute('class', 'group col-sm-6');
+        
+        $this->setWidth('col-sm-6');
     }
     
     
     /** {@inheritDoc} */
     public function getText()
     {
-        return _("Category");
+        return _('Category');
     }
 
 }
