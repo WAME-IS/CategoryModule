4a09570aefe3fbc6e3bd5804244adb00c60d43ba
shadd0w <rene.gmitter@wame.sk>
2016-10-28 10:33:53 +0200

    RENAME AbastractComponentPresenter to AbstractComponentPresenter

--- vendor/wame/AdminModule/presenters/CategoryFilterControlPresenter.php
+++ vendor/wame/AdminModule/presenters/CategoryFilterControlPresenter.php
@@ -5,7 +5,7 @@ namespace App\AdminModule\Presenters;
 use Wame\Core\Presenters\Traits\UseParentTemplates;
 
 
-class CategoryFilterControlPresenter extends AbastractComponentPresenter
+class CategoryFilterControlPresenter extends AbstractComponentPresenter
 {	
     use UseParentTemplates;
     
